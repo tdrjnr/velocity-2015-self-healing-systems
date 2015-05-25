1d3ee4d8123ed99998a3ca4fd2460f2beb1055f4
Matt Solnit <msolnit@soasta.com>
2015-05-25 16:57:39 -0700

    New version that parses CloudWatch alarm JSON.  Removed no-longer-used code for integrating with Kinesis and custom SNS message format. Also changed the set_address_enabled() function to take a callback instead of directly manipulating the Lambda context.

--- dynect/enable_td_address_lambda.js
+++ dynect/enable_td_address_lambda.js
@@ -1,34 +1,97 @@
 var dynect = require('./dynect_api.js');
+var AWS = require('aws-sdk');
 
 var debug = true;
 
-exports.sns_handler = function(event, context) {
+exports.cloudwatch_alarm_sns_handler = function(event, context) {
 	debug_log(JSON.stringify(event, null, 2));
 
 	event.Records.forEach(function(record) {
 		var payload = record.Sns.Message;
-		lambda_handler(payload, context);
+		debug_log('Payload:' + payload);
+
+		// Example of expected alarm JSON:
+		//
+		// {
+		//	"AlarmName": "High Error Count",
+		//	"AlarmDescription": "Alerts when ERROR rate exceeds healthy limits",
+		//	"AWSAccountId": "XXXX",
+		//	"NewStateValue": "ALARM",
+		//	"NewStateReason": "Threshold Crossed: 1 datapoint (6.0) was greater than or equal to the threshold (6.0).",
+		//	"StateChangeTime": "2015-05-24T21:40:12.882+0000",
+		//	"Region": "US-West-2",
+		//	"OldStateValue": "OK",
+		//	"Trigger": {
+		//		"MetricName": "ERRORLogLines",
+		//		"Namespace": "SHSAppMetrics",
+		//		"Statistic": "AVERAGE",
+		//		"Unit": null,
+		//		"Dimensions": [
+		//		{
+		//			"name": "InstanceId",
+		//			"value": "i-ff04cb29"
+		//		}],
+		//		"Period": 60,
+		//		"EvaluationPeriods": 1,
+		//		"ComparisonOperator": "GreaterThanOrEqualToThreshold",
+		//		"Threshold": 6
+		//	}
+		// }
+		var alarm = JSON.parse(payload);
+
+		// Extract the instance status.  ALARM means it's down, OK means it's up.
+		var instance_up = (alarm.NewStateValue === "ALARM" ? "false" : "true");
+
+		var instance_id;
+		var instance_public_ip_address;
+
+		// Extract the instance ID from the dimension list.
+		alarm.Trigger.Dimensions.forEach(function(dimension) {
+			if (dimension.name === "InstanceId") {
+				instance_id = dimension.value;
+			}
+		});
+
+		// To modify the DynECT record, we need the public IP address of the instance.
+		// Get it from EC2.
+		new AWS.EC2().describeInstances({ InstanceIds: [ instance_id ] }, function(error, data) {
+			if (error) {
+				// EC2 API call failed.
+				debug_log(error);
+
+				// Nothing else we can do.  Report the failure to Lambda.
+				context.fail(error);
+			}
+			else {
+				// EC2 API call succeeded.
+
+				// Extract the public IP address from the response.
+				instance_public_ip_address = data.Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicIp;
+
+				debug_log("Instance ID: " + instance_id);
+				debug_log("Instance public IP address: " + instance_public_ip_address);
+
+				// Wrap up the params needed to adjust the DynECT record.
+				var args = {
+					"customer_name": "",
+					"user_name": "",
+					"password": "",
+					"service_label": "",
+					"address": instance_public_ip_address,
+					"enabled": instance_up
+				};
+
+				// Do it!
+				set_address_enabled(args, function() {
+					// Success!
+					context.succeed();
+				});
+			}
+		});
 	});
 }
 
-exports.kinesis_handler = function(event, context) {
-	debug_log(JSON.stringify(event, null, 2));
-
-	event.Records.forEach(function(record) {
-		// Kinesis data is base64 encoded so decode here
-		var payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
-		lambda_handler(payload, context);
-	});
-}
-
-lambda_handler = function(payload, context) {
-	debug_log('Payload:' + payload);
-
-	var args = JSON.parse(payload);
-	set_address_enabled(args, context);
-}
-
-set_address_enabled = function(args, context) {
+set_address_enabled = function(args, callback) {
 	var customer_name = args.customer_name;
 	var user_name = args.user_name;
 	var password = args.password;
@@ -70,7 +133,10 @@ set_address_enabled = function(args, context) {
 
 				dynect.update_dsf_record(token, service_id, record_id, updated_record, function(dsf_record) {
 					debug_log("Success!");
-					context.succeed();
+
+					if (callback) {
+						callback();
+					}
 				});
 			});
 		});
