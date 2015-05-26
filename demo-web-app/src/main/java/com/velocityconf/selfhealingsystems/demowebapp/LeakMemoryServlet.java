89cd4b296cdb99d49294ff55b4119e065035f6cf
Matt Solnit <msolnit@soasta.com>
2015-05-26 14:11:53 -0700

    Add a new "release leaked memory" option to the demo app.  Also experimenting with soft references.  We'll see whether this helps or hurts with generating fake leak scenarios.

--- demo-web-app/src/main/java/com/velocityconf/selfhealingsystems/demowebapp/LeakMemoryServlet.java
+++ demo-web-app/src/main/java/com/velocityconf/selfhealingsystems/demowebapp/LeakMemoryServlet.java
@@ -1,74 +1,94 @@
 package com.velocityconf.selfhealingsystems.demowebapp;
 
 import java.io.*;
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class LeakMemoryServlet extends HttpServlet
 {
-  private static final List<byte[]> leakedObjects = new ArrayList<>();
+  private static final List<SoftReference<byte[]>> leakedObjects = new ArrayList<>();
+  private static final AtomicLong leakedByteCount = new AtomicLong();
+
+  private Log log = LogFactory.getLog(getClass());
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
   {
-    String blockCountParam = request.getParameter("blockCount");
-    String blockSizeParam = request.getParameter("blockSize");
-    String delayBetweenBlocksParam = request.getParameter("delayBetweenBlocks");
+    boolean releaseLeakedMemory = Boolean.parseBoolean(request.getParameter("release"));
 
-    // Example usage:
-    // http://localhost:8080/shs-demo-app/LeakMemory?blockCount=5&blockSize=1000000000&delayBetweenBlocks=500
-
-    int blockCount = 128;
-    int blockSize = 1024;
-    int delayBetweenBlocks = 1000;
-
-    if (blockCountParam != null && !blockCountParam.isEmpty())
+    if (releaseLeakedMemory)
     {
-      blockCount = Integer.parseInt(request.getParameter("blockCount"));
+      log.info("Releasing " + leakedByteCount + " leaked bytes.");
+      leakedObjects.clear();
+      leakedByteCount.set(0);
     }
-    if (blockSizeParam != null && !blockSizeParam.isEmpty())
+    else
     {
-      blockSize = Integer.parseInt(request.getParameter("blockSize"));
-    }
-    if (delayBetweenBlocksParam != null && !delayBetweenBlocksParam.isEmpty()) 
-    {
-      delayBetweenBlocks = Integer.parseInt(request.getParameter("delayBetweenBlocks"));
-    } 
+      String blockCountParam = request.getParameter("blockCount");
+      String blockSizeParam = request.getParameter("blockSize");
+      String delayBetweenBlocksParam = request.getParameter("delayBetweenBlocks");
 
-    response.getWriter().println("<html>");
-    response.getWriter().println("<head>");
-    response.getWriter().println("<link rel='icon' type='image/png' href='velocity_favicon.png'>");
-    response.getWriter().println("</head>");
-    response.getWriter().println("<body>");
+      // Example usage:
+      // http://localhost:8080/shs-demo-app/LeakMemory?blockCount=5&blockSize=1000000000&delayBetweenBlocks=500
 
-    for (int i = 0; i < blockCount; i++)
-    {
-      leakedObjects.add(new byte[blockSize]);
+      int blockCount = 128;
+      int blockSize = 1024;
+      int delayBetweenBlocks = 1000;
 
-      response.getWriter().println("Just leaked " + blockSize + " bytes. " + (blockCount - i - 1) + " block(s) remaining. Will sleep for " + delayBetweenBlocks + " ms...");
-      response.getWriter().println("<br />");
-      response.getWriter().flush();
-
-      if (delayBetweenBlocks > 0)
+      if (blockCountParam != null && !blockCountParam.isEmpty())
       {
-        try
+        blockCount = Integer.parseInt(request.getParameter("blockCount"));
+      } 
+      if (blockSizeParam != null && !blockSizeParam.isEmpty())
+      {
+        blockSize = Integer.parseInt(request.getParameter("blockSize"));
+      }
+      if (delayBetweenBlocksParam != null && !delayBetweenBlocksParam.isEmpty()) 
+      {
+        delayBetweenBlocks = Integer.parseInt(request.getParameter("delayBetweenBlocks"));
+      } 
+
+      response.getWriter().println("<html>");
+      response.getWriter().println("<head>");
+      response.getWriter().println("<link rel='icon' type='image/png' href='velocity_favicon.png'>");
+      response.getWriter().println("</head>");
+      response.getWriter().println("<body>");
+
+      for (int i = 0; i < blockCount; i++)
+      {
+        leakedObjects.add(new SoftReference(new byte[blockSize]));
+        leakedByteCount.addAndGet((long)blockSize);
+
+        response.getWriter().println("Just leaked " + blockSize + " bytes. " + (blockCount - i - 1) + " block(s) remaining. Will sleep for " + delayBetweenBlocks + " ms...");
+        response.getWriter().println("<br />");
+        response.getWriter().flush();
+
+        if (delayBetweenBlocks > 0)
         {
-          Thread.sleep(delayBetweenBlocks);
-        }
-        catch (InterruptedException e)
-        {
-          // For demo purposes, no need to handle this.
+          try
+          {
+            Thread.sleep(delayBetweenBlocks);
+          }
+          catch (InterruptedException e)
+          {
+            // For demo purposes, no need to handle this.
+          }
         }
       }
+
+      response.getWriter().println("Done!");
+
+      response.getWriter().println("</body>");
+      response.getWriter().println("</html>");
+      response.getWriter().flush();
     }
-
-    response.getWriter().println("Done!");
-
-    response.getWriter().println("</body>");
-    response.getWriter().println("</html>");
-    response.getWriter().flush();
   }
 }
